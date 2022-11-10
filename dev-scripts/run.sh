b2a120ff430d574f909d5ba4d851a64dced315fd
Jerry Soong <60661834+jjsoong@users.noreply.github.com>
2022-11-10 16:19:49 -0500

    Updated react to use optimised build for docker - added Dockerfile.prod that uses react-scripts build - added nginx.conf - updated mount destination paths in run.sh

--- dev-scripts/run.sh
+++ dev-scripts/run.sh
@@ -1,5 +1,7 @@
 # Runs the three docker images in containers
-sudo docker run -d --name react_cont --network=dnet --mount type=bind,source="$(pwd)"/../react-website/public/themes-dist,target=/home/app/public/themes-dist --mount type=bind,source="$(pwd)"/../react-website/public/wet-boew-dist,target=/home/app/public/wet-boew-dist react
+# sudo docker run -d --name react_cont --network=dnet --mount type=bind,source="$(pwd)"/../react-website/public/themes-dist,target=/home/app/public/themes-dist --mount type=bind,source="$(pwd)"/../react-website/public/wet-boew-dist,target=/home/app/public/wet-boew-dist react
+sudo docker run -d --name react_cont --network=dnet --mount type=bind,source="$(pwd)"/../react-website/public/themes-dist,target=/usr/share/nginx/html/themes-dist --mount type=bind,source="$(pwd)"/../react-website/public/wet-boew-dist,target=/usr/share/nginx/html/wet-boew-dist react
+
 sudo docker run -d --name api_cont --network=dnet api
 
 # Since react needs some time to start up, sleep for 1 mins
